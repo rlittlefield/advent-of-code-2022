82ea8cd2ee237c3e61c065b17f250307786e3104
Ryan Littlefield <ryan@ryanlittlefield.com>
2022-12-07 23:44:20 -0700

    fixed day7 issue where it accumulated both children and grandchildren directory sizes

--- src/day7p1.rs
+++ src/day7p1.rs
@@ -1,31 +1,30 @@
-
-#[path = "reader.rs"] mod reader;
+#[path = "reader.rs"]
+mod reader;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::io::BufReader;
 use std::io::Read;
-use std::collections::VecDeque;
 
 extern crate itertools;
 use itertools::Itertools;
 use itertools::PutBack;
 
+use core::cmp::Ord;
 use itertools::put_back;
+use std::cell::RefCell;
+use std::fs;
 use std::fs::File;
 use std::io::{self, BufRead, Lines};
 use std::path::Path;
-use std::fs;
-use std::cell::RefCell;
-
 
 use std::rc::Rc;
-
 
 #[derive(Debug)]
 enum Command {
     Back,
     Forward(String),
-    List
+    List,
 }
 
 fn parse_command(line: String) -> Command {
@@ -37,7 +36,7 @@ fn parse_command(line: String) -> Command {
         match value {
             ".." => {
                 return Command::Back;
-            },
+            }
             val => {
                 return Command::Forward(val.to_owned());
             }
@@ -46,16 +45,17 @@ fn parse_command(line: String) -> Command {
         return Command::List;
     }
     panic!("uh oh, this didn't look like a valid command");
-    
 }
 
-pub fn handle_commands(lines: Rc<RefCell<PutBack<Lines<BufReader<File>>>>>, directory_name: String) -> Vec<(String, u64, Vec<(String, u64)>)> {
+pub fn handle_commands(
+    lines: Rc<RefCell<PutBack<Lines<BufReader<File>>>>>,
+    directory_name: String,
+) -> (u64, Vec<(String, u64, Vec<(String, u64)>)>) {
     let mut accumulator: Vec<(String, u64, Vec<(String, u64)>)> = vec![];
     // we return the accumulator, but the own_size is basically added into the accumulator if we ever do an ls in here
     let mut own_size: u64 = 0;
     let mut files: Vec<(String, u64)> = vec![];
     loop {
-        
         let line = {
             if let Some(Ok(line)) = lines.borrow_mut().next() {
                 Some(line)
@@ -67,21 +67,25 @@ pub fn handle_commands(lines: Rc<RefCell<PutBack<Lines<BufReader<File>>>>>, dire
             match parse_command(line) {
                 Command::Forward(dir) => {
                     println!("changing to {} directory", dir);
-                    let result = handle_commands(Rc::clone(&lines), format!("{}/{}", directory_name, dir));
+                    let (child_size, result) =
+                        handle_commands(Rc::clone(&lines), format!("{}/{}", directory_name, dir));
                     let mut subdir_size = 0;
+                    own_size += child_size; // omg, this should have been obvious
                     for (subdir, size, files) in result {
-                        subdir_size += size;
                         accumulator.push((subdir, size, files));
                     }
-                    println!("adding {} bytes to directory {} from child {}", subdir_size, directory_name, dir);
+                    println!(
+                        "adding {} bytes to directory {} from child {}",
+                        subdir_size, directory_name, dir
+                    );
                     own_size += subdir_size;
-                },
+                }
                 Command::Back => {
                     println!("moving back a directory from {}", directory_name);
                     break; // this breaks out of the main loop, which returns the accumulator and everything so we don't need to do it here
-                    // accumulator.push((directory_name.clone(), own_size));
-                    // return accumulator;
-                },
+                           // accumulator.push((directory_name.clone(), own_size));
+                           // return accumulator;
+                }
                 Command::List => {
                     // this needs to collect everything until a new line starts with a $ again
                     // println!("listed directory {}", directory_name);
@@ -103,7 +107,7 @@ pub fn handle_commands(lines: Rc<RefCell<PutBack<Lines<BufReader<File>>>>>, dire
                                     let mut borrowed_lines = lines.borrow_mut();
                                     borrowed_lines.put_back(Ok(line));
                                 }
-                                break; 
+                                break;
                                 // this line isn't a response, it is a new command. so we just put it back and break
                                 // which will pop out of the List loop, and continue the outer line loop
                             }
@@ -117,51 +121,63 @@ pub fn handle_commands(lines: Rc<RefCell<PutBack<Lines<BufReader<File>>>>>, dire
                             files.push((filename.to_owned(), size));
                             // let filename = filename.to_owned();
                             own_size += size;
-                            println!("found file in directory {} with size: {}", directory_name, size);
-
-        
+                            println!(
+                                "found file in directory {} with size: {}",
+                                directory_name, size
+                            );
                         } else {
                             // I guess this means the file is empty?
                             println!("file empty at end of list");
                             break;
                         }
                     }
-                    
                 }
             }
         } else {
             break;
         }
     }
-    println!("finished with directory {} with size {}", directory_name, own_size);
+    println!(
+        "finished with directory {} with size {}",
+        directory_name, own_size
+    );
     accumulator.push((directory_name.clone(), own_size, files));
-    return accumulator;
+    return (own_size, accumulator);
 }
 
-
-
 pub fn main() {
-
-    if let Ok(mut lines) = reader::read_lines("./input7") {        
+    if let Ok(mut lines) = reader::read_lines("./input7") {
         lines.next();
         let lines = put_back(lines);
         let lines = Rc::new(RefCell::new(lines));
-        let mut result = handle_commands(lines, String::from(""));
-
-        result.sort();
+        let (total_size, mut result) = handle_commands(lines, String::from(""));
+        println!("total size: {}", total_size);
+        // result.sort();
         print!("{:#?}", result);
-        let matching_items = result.into_iter().filter(|(_dir, size, _files)| *size <= 100000).collect_vec();
+        // let matching_items = result.into_iter().filter(|(_dir, size, _files)| *size <= 100000).collect_vec();
         // print!("{:#?}", matching_items);
 
-        let matching_sizes = matching_items.into_iter().filter(|(_dir, size, _files)| *size <= 100000).map(|(_dir, size, _files)| size).collect_vec();
-        
-        
+        // let matching_sizes = matching_items.into_iter().filter(|(_dir, size, _files)| *size <= 100000).map(|(_dir, size, _files)| size).collect_vec();
+
         // println!("{:#?}", matching_sizes);
-        let sum: u64 = matching_sizes.into_iter().sum();
-        println!("sum of < 100000: {}", sum);
+        // let sum: u64 = matching_sizes.into_iter().sum();
+        // println!("sum of < 100000: {}", sum);
 
         // print!("{:#?}", result.into_iter().filter(|(_dir, size)| size.to_owned() < 100000).collect_vec());
 
+        let disk_total = 70000000;
+        let available_space = disk_total - total_size;
+        let update_size = 30000000;
+        let additional_space_required = update_size - available_space;
 
+        let matching_sorted_dirs = result
+            .into_iter()
+            .filter(|(_, size, _)| size >= &additional_space_required)
+            .sorted_by(|a, b| Ord::cmp(&b.1, &a.1))
+            .collect_vec();
+        println!("{:#?}", matching_sorted_dirs)
+        //sorted_by(|_name, size, _files| size)
+
+        // filter(|(_dir, size, _files)| *size <= 100000)
     }
 }
--- src/reader.rs
+++ src/reader.rs
@@ -1,21 +1,22 @@
-
+use std::fs;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::fs;
 
 // found this somewhere online in rust docs. lines() is nice
 pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-
-
 // this is like the same as above, but we are trying to do bytes strings not unicodes
 pub fn read_byte_lines<P>(filename: P) -> io::Result<io::Split<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).split(b'\n'))
 }
